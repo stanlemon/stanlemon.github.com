e527bcab2fd02e8097c08abf6a78430390f812a5
Stan Lemon <stanlemon@users.noreply.github.com>
2020-12-04 19:50:25 -0500

    Lots of cleanup

--- _data/site.js
+++ _data/site.js
@@ -1,14 +1,14 @@
 module.exports = () => ({
-    name: "lemonbytes",
-    title: "lemonbytes by Stan Lemon",
-    author: "Stan Lemon",
-    description: "husband, dad, steelers fan and software engineer",
-    social: {
-        twitter: "stanlemon",
-        github: "stanlemon",
-        linkedin: "stanlemon"
-    },
-    google_analytics: "UA-33072694-1",
-    // If an env variable (local dev) has been set, use it, otherwise default (prod)
-    url:  process.env.SITE_URL ? process.env.SITE_URL : "https://stanlemon.com"
+  name: "lemonbytes",
+  title: "lemonbytes by Stan Lemon",
+  author: "Stan Lemon",
+  description: "husband, dad, steelers fan and software engineer",
+  social: {
+    twitter: "stanlemon",
+    github: "stanlemon",
+    linkedin: "stanlemon",
+  },
+  google_analytics: "UA-33072694-1",
+  // If an env variable (local dev) has been set, use it, otherwise default (prod)
+  url: process.env.SITE_URL ? process.env.SITE_URL : "https://stanlemon.com",
 });
